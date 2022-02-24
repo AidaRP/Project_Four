80aa65529203aef881f46c3d130da0334c001c98
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-02-24 10:42:02 +0100

    Updating of view of UserRouter

--- views/UserRouter.js
+++ views/UserRouter.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middlewares/auth.js');
-
 const UserController = require('../controllers/UserController');
+const { authentication , isAdmin} = require('../middlewares/authentication');
 
-
-
+router.post('/',UserController.create)
+router.get('/',UserController.getAll)
+router.delete('/id/id:', authentication , isAdmin, UserController.delete)
+router.put('/id:id',UserController.update)
+router.post('/login',UserController.login)
+router.delete('/logout',authentication, UserController.logout)
 
 module.exports = router;
+
+
+
+
+
+
