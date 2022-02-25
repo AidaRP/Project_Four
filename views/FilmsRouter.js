0fada6f41cba5e842d74c0615c567fe1139cbe69
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-02-25 10:27:09 +0100

    Updating and refactorizing endpoints of FilmsRouter

--- views/FilmsRouter.js
+++ views/FilmsRouter.js
@@ -1,10 +1,30 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
-const auth = require('../middlewares/auth.js');
 
 const FilmsController = require('../controllers/FilmsController');
 
 
+//DECLARE CRUD RESTFULL
 
+//Read all films
+router.get('/',FilmsController.getFilms);
+//http://localhost:3000/peliculas
+
+//Register a new film
+router.post('/', FilmsController.filmRegister);
+//http://localhost:3000/peliculas
+
+//Search films for favorite category
+router.get('/favorite', FilmsController.favouriteFilms);
+
+//Search new films for  adult category
+router.get('/adults', FilmsController.adultFilms);
+
+//Search films for her title
+router.get('/title', FilmsController.titleFilms);
+
+//Seacrh a News Films
+router.get('/news', FilmsController.traeNovedades);
 
 module.exports = router;
